03e1b6a0122c9b04d5c317c151ef0ad19488c07c
Fabio Spampinato <spampinabio@gmail.com>
2024-03-28 23:03:24 +0000

    Optimized matching some simple globs, speeding things up by ~10%

--- src/glob/matcher.ts
+++ src/glob/matcher.ts
@@ -1,12 +1,15 @@
 
 /* IMPORT */
 
+import escapeRegExp from 'string-escape-regex';
 import parse from './parse';
 
 /* HELPERS */
 
 const STAR_RE = /^\*+$/;
 const STATIC_RE = /^[ a-zA-Z0-9/._-]*$/;
+const FLEXIBLE_START_RE = /^\*+([ a-zA-Z0-9/._-]*)$/;
+const FLEXIBLE_END_RE = /^([ a-zA-Z0-9/._-]*)\*+$/;
 
 /* MAIN */
 
@@ -33,7 +36,47 @@ const matcher = ( glob: string, caseSensitive: boolean ): (( segment: string ) =
       const globLowerCase = glob.toLowerCase ();
       const globLength = globLowerCase.length;
 
-      return ( segment: string ) => segment.length === globLength && segment.toLowerCase () === globLowerCase;
+      return ( segment: string ) => segment.length === globLength && ( segment.toLowerCase () === globLowerCase );
+
+    }
+
+  }
+
+  /* FLEXIBLE START SPECIAL-CASE */
+
+  const end = FLEXIBLE_START_RE.exec ( glob )?.[1];
+
+  if ( end ) {
+
+    if ( caseSensitive ) {
+
+      return ( segment: string ) => segment.endsWith ( end );
+
+    } else {
+
+      const re = new RegExp ( `${escapeRegExp ( end )}$`, 'i' );
+
+      return ( segment: string ) => re.test ( segment );
+
+    }
+
+  }
+
+  /* FLEXIBLE END SPECIAL-CASE */
+
+  const start = FLEXIBLE_END_RE.exec ( glob )?.[1];
+
+  if ( start ) {
+
+    if ( caseSensitive ) {
+
+      return ( segment: string ) => segment.startsWith ( start );
+
+    } else {
+
+      const re = new RegExp ( `^${escapeRegExp ( start )}`, 'i' );
+
+      return ( segment: string ) => re.test ( segment );
 
     }
 
