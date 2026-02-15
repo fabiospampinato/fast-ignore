ef60d87a5e0e9176f099e4b06f6670abc25e4eb2
Fabio Spampinato <spampinabio@gmail.com>
2026-02-15 12:43:22 +0000

    Tick: avoiding duplicating the input array of nodes

--- src/ignore/tick.ts
+++ src/ignore/tick.ts
@@ -5,59 +5,65 @@ import type {Node, Tick} from '../types';
 
 /* MAIN */
 
+// This function basically moves a pointer forward on the trie by just 1 non-globstar step, if possible
+
+const tickNode = ( result: Tick, node: Node, segment: string ): void => {
+
+  const {children} = node;
+
+  for ( let i = 0, l = children.length; i < l; i++ ) { // Matching children
+
+    const child = children[i];
+
+    if ( !child.match ( segment ) ) continue;
+
+    if ( child.strength >= result.strength ) { // Stronger result found
+
+      result.negative = child.negative;
+      result.strength = child.strength;
+
+    }
+
+    if ( child.children.length ) { // Generating next and forked pointers, if we have somewhere to go
+
+      if ( child.globstar ) { // Keep going for this pointer within this tick
+
+        tickNode ( result, child, segment );
+
+      } else { // Stopping for this pointer within this tick
+
+        result.nodes.push ( child );
+
+      }
+
+    }
+
+  }
+
+  if ( node.globstar ) { // Keep matching self
+
+    if ( node.strength >= result.strength ) { // Stronger result found
+
+      result.negative = node.negative;
+      result.strength = node.strength;
+
+    }
+
+    result.nodes.push ( node );
+
+  }
+
+};
+
 // This function basically moves each pointer forward on the trie by just 1 non-globstar step, if possible
 
-const tick = ( _nodes: Node[], segment: string ): Tick => {
+const tick = ( nodes: Node[], segment: string ): Tick => {
 
   const result: Tick = { nodes: [], negative: false, strength: -1 };
-  const nodes: Node[] = _nodes.slice ();
 
-  for ( let ni = 0; ni < nodes.length; ni++ ) { // Ticking from each node
+  for ( let i = 0, l = nodes.length; i < l; i++ ) { // Ticking from each node
 
-    const node = nodes[ni];
-    const {children} = node;
-
-    for ( let ci = 0, cl = children.length; ci < cl; ci++ ) { // Matching children
-
-      const nodeNext = children[ci];
-
-      if ( !nodeNext.match ( segment ) ) continue;
-
-      if ( nodeNext.strength >= result.strength ) { // Stronger result found
-
-        result.negative = nodeNext.negative;
-        result.strength = nodeNext.strength;
-
-      }
-
-      if ( nodeNext.children.length ) { // Generating next and forked pointers, if we have somewhere to go
-
-        if ( nodeNext.globstar ) { // Keep going for this pointer within this tick
-
-          nodes.push ( nodeNext );
-
-        } else { // Stopping for this pointer within this tick
-
-          result.nodes.push ( nodeNext );
-
-        }
-
-      }
-
-    }
-
-    if ( node.globstar ) { // Keep matching self
-
-      if ( node.strength >= result.strength ) { // Stronger result found
-
-        result.negative = node.negative;
-        result.strength = node.strength;
-
-      }
-
-      result.nodes.push ( node );
-
-    }
+    tickNode ( result, nodes[i], segment );
 
   }
 
